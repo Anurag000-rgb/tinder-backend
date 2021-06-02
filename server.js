04680108ef4c715d25597b9346e522319ecf9190
Anurag000-rgb <22anurag44@gmail.com>
2021-06-02 23:29:37 +0530

    tinder-backend

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Cards from './dbCards.js';
+import Cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 8000
+const connection_url = `mongodb+srv://admin:4jso7onYUxJCZD3c@cluster0.t5cls.mongodb.net/tinderdb?retryWrites=true&w=majority`;
+
+app.use(express.json());
+app.use(Cors());
+
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!!!')
+})
+
+app.post('/tinder/card', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
+
+app.get('/tinder/card', (req, res) => {
+    Cards.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`);
+})
